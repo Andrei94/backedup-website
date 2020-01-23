ce7dfbef8f328fc76b4bf028d0eff31a13e5c59a
Andrei Precup <andreiprecup69@gmail.com>
2020-01-23 16:59:48 +0200

    Main page first draft with navbar, carousel and pricing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'backedup-website';
+  slides = [
+    'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+    'https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg'
+  ];
 }
