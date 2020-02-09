252ef1e12706ceed332d4d8769c02faf6b495d5f
Andrei Precup <andreiprecup69@gmail.com>
2020-02-09 15:34:11 +0200

    Add first name, last name and phone number to sign up

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -13,26 +13,46 @@ export class AuthenticationComponent implements OnInit {
     hideAllDefaults: true,
     signUpFields: [
       {
+        label: 'First Name',
+        key: 'given_name',
+        required: true,
+        displayOrder: 1,
+        type: 'string'
+      },
+      {
+        label: 'Last Name',
+        key: 'family_name',
+        required: true,
+        displayOrder: 2,
+        type: 'string'
+      },
+      {
         label: 'Username',
         key: 'username',
         required: true,
-        displayOrder: 1,
+        displayOrder: 3,
         type: 'string',
       },
       {
         label: 'Email',
         key: 'email',
         required: true,
-        displayOrder: 2,
+        displayOrder: 4,
         type: 'string',
+      },
+      {
+        label: 'Phone Number',
+        key: 'phone_number',
+        required: true,
+        displayOrder: 5,
+        type: 'string'
       },
       {
         label: 'Password',
         key: 'password',
         required: true,
-        displayOrder: 3,
-        type: 'password',
-        invalid: false
+        displayOrder: 6,
+        type: 'password'
       }
     ]
   };
