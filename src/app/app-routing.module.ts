1ed80d89e9f112802965b1864bd3fa94b9965517
Andrei Precup <andreiprecup69@gmail.com>
2020-02-17 12:13:50 +0200

    Moved aws dependent features to a separate module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ProfileComponent} from './profile/profile.component';
+import {ProfileComponent} from './aws/profile/profile.component';
 import {HomeComponent} from './home/home.component';
-import {AuthenticationComponent} from './authentication/authentication.component';
+import {AuthenticationComponent} from './aws/authentication/authentication.component';
 import {PaymentComponent} from './payment/payment.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'auth', component: AuthenticationComponent},
-  {path: 'profile', component: ProfileComponent},
+  {path: 'auth', component: AuthenticationComponent, loadChildren: () => import('./aws/aws.module').then(m => m.AwsModule)},
+  {path: 'profile', component: ProfileComponent, loadChildren: () => import('./aws/aws.module').then(m => m.AwsModule)},
   {path: 'pay/:customerId/:planId', component: PaymentComponent}
 ];
 
