3a81adb9aa1e206be5f1a51ec73446ba7ec74327
Andrei Precup <andreiprecup69@gmail.com>
2020-02-19 17:18:19 +0200

    POC random number generator for uid and gid

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,5 +10,23 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    let acc = new Map();
+    for (let i = 0; i < 100000; i++) {
+      let message = this.random();
+      if (acc.has(message.substr(message.length - 7))) {
+        acc.set(message.substr(message.length - 7), acc.get(message.substr(message.length - 7)) + 1);
+      } else {
+        acc.set(message.substr(message.length - 7), 1);
+      }
+    }
+    acc.forEach((value, key) => {
+      if (acc.get(key) > 1) {
+        console.log({key, value})
+      }
+    });
+  }
+
+  private random(): string {
+    return new Date().getTime() + '' + Math.floor(Math.random() * 10000000)
   }
 }
