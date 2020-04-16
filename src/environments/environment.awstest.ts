70ce191def4d2531ed582b21810bba97836f71b3
Andrei Precup <andreiprecup69@gmail.com>
2020-04-16 15:46:43 +0300

    Created aws test environment with api gateway

---
+++ src/environments/environment.awstest.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  paySubscriptionUrl: 'https://i9bdhatjq3.execute-api.eu-central-1.amazonaws.com/test/payment/pay-subscription',
+  generateClientTokenUrl: 'https://i9bdhatjq3.execute-api.eu-central-1.amazonaws.com/test/payment/generate-client-token',
+  createCustomerUrl: 'https://i9bdhatjq3.execute-api.eu-central-1.amazonaws.com/test/payment/create-customer'
+};
