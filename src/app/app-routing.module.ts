643508135df7d729519d52a6be83c62e1c6aedc6
Andrei Precup <andreiprecup69@gmail.com>
2020-04-22 14:30:14 +0300

    Removed the routing configuration for log in, signup and payment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ProfileComponent} from './profile/profile.component';
 import {HomeComponent} from './home/home.component';
-import {AuthenticationComponent} from './authentication/authentication.component';
-import {PaymentComponent} from './payment/payment.component';
 import {ContactUsComponent} from './contact-us/contact-us.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'auth', component: AuthenticationComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'pay/:customerId/:planId', component: PaymentComponent},
   {path: 'contact', component: ContactUsComponent}
 ];
 
