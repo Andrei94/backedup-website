2840be4c506c22f34209badd54c894bd60d34fab
Andrei Precup <andreiprecup69@gmail.com>
2020-04-26 15:20:21 +0300

    Added ng-toolkit for SEO

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,37 +1,24 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserTransferStateModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AppRoutingModule} from './app-routing.module';
-import {HomeComponent} from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
-import {ContactUsComponent} from './contact-us/contact-us.component';
 import {FormsModule} from '@angular/forms';
 import {RECAPTCHA_SETTINGS, RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings} from 'ng-recaptcha';
-import {CommonModule} from '@angular/common';
-import {TransferHttpCacheModule} from '@nguniversal/common';
-import {NgtUniversalModule} from '@ng-toolkit/universal';
-import {NgxGoogleAnalyticsModule} from 'ngx-google-analytics';
+import {AppModule} from './app.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ContactUsComponent
-  ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'serverApp'}),
     NgbModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     RecaptchaModule,
     RecaptchaFormsModule,
-    CommonModule,
-    TransferHttpCacheModule,
-    NgtUniversalModule,
-    NgxGoogleAnalyticsModule.forRoot('UA-157507530-1')
+    AppModule,
+    BrowserTransferStateModule,
   ],
   providers: [{
     provide: RECAPTCHA_SETTINGS,
@@ -41,5 +28,5 @@ import {NgxGoogleAnalyticsModule} from 'ngx-google-analytics';
   }],
   bootstrap: [AppComponent]
 })
-export class AppModule {
+export class AppBrowserModule {
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +7,4 @@ import {NavigationEnd, Router} from '@angular/router';
 })
 export class AppComponent {
   title = 'backedup-website';
-
-  constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        (window as any).ga('set', 'page', event.urlAfterRedirects);
-        (window as any).ga('send', 'pageview');
-      }
-    });
-  }
-
 }
