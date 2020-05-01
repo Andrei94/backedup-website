4dc55921bc1d2b99c91aca59a2e0d91e6bbc9d6b
Andrei Precup <andreiprecup69@gmail.com>
2020-05-01 13:55:01 +0300

    Added aws prod environment

---
+++ src/environments/environment.awsprod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  siteKey: '6LcGqvAUAAAAADoAe0iizAxTwvXRYAXexLth1n9f',
+  contactUsEmail: 'https://i9bdhatjq3.execute-api.eu-central-1.amazonaws.com/v1/contact-us-email',
+  notifySubscription: 'https://i9bdhatjq3.execute-api.eu-central-1.amazonaws.com/v1/notifiable-customer'
+};
